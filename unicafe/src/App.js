2bbe6a4e2cc757039a37c945ebc3b54c5e680366
Paul Ollivier <paulollivier1990@gmail.com>
2021-03-28 14:04:36 +1030

    Exercise 1.7 Completed

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -19,6 +19,8 @@ const App = () => {
   const handleBadClick = () => {
     setBad(bad + 1)
   }
+
+  
   return (
     <div>
       <h1>give feedback</h1>
@@ -29,6 +31,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p> all {good+bad+neutral}</p>
+      <p>average {(good - bad) / (good+bad+neutral)}</p>
+      <p>positive {good / (good+bad+neutral) * 100} %</p> 
     </div>
   )
 }
