ee324964477c3c0c88bb21ae4736f4e26d788895
Paul Ollivier <paulollivier1990@gmail.com>
2021-03-28 14:12:34 +1030

    Refactored with All variable and state

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -2,16 +2,16 @@ import "./App.css"
 
 import React, { useState } from "react"
 
-const Statistics = ({good, bad, neutral}) => {
+const Statistics = ({good, bad, neutral, all}) => {
   return (
     <div>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      <p> all {good + bad + neutral}</p>
-      <p>average {(good - bad) / (good + bad + neutral)}</p>
-      <p>positive {(good / (good + bad + neutral)) * 100} %</p>
+      <p> all {all}</p>
+      <p>average {(good - bad) / all}</p>
+      <p>positive {(good / all) * 100} %</p>
     </div>
   )
 }
@@ -21,17 +21,21 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGoodClick = () => {
     setGood(good + 1)
+    setAll(all + 1)
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setAll(all + 1)
   }
 
   return (
@@ -40,7 +44,7 @@ const App = () => {
       <button onClick={handleGoodClick}>good</button>
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
-      <Statistics good={good} bad={bad} neutral={neutral} />
+      <Statistics good={good} bad={bad} neutral={neutral} all={all} />
     </div>
   )
 }
